343cf74ef85f0a000722f2673189cb3771632c0c
Keku Belina <kekubt@gmail.com>
2023-02-10 17:58:46 -0500

    edit3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
@@ -23,18 +21,17 @@ if auth_type == 'auth':
 if auth_type == 'basic_auth':
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """
-        Not found handler.
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """
-        Unauthorized handler.
+    """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
@@ -48,10 +45,7 @@ def forbidden(error) -> str:
 
 @app.before_request
 def authenticate_user():
-    """
-    Authenticates a user before processing a request.
-    auth_header variable is used to retrieve the authorization header.
-    user variable is used to get the current user associated with the request
+    """Authenticates a user before processing a request.
     """
     if auth:
         excluded_paths = [
@@ -67,8 +61,8 @@ def authenticate_user():
             if user is None:
                 abort(403)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
