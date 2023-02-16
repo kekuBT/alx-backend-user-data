2678d59f265005c27be8b1865d3cccfb90c9113b
Keku Belina <kekubt@gmail.com>
2023-02-15 20:21:27 -0500

    edit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'users'
